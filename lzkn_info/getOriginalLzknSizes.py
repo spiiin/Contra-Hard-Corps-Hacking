ceec14c1546876b78496345881dae9823175fa40
spiiin <sanya.boyko@gmail.com>
2017-12-11 22:54:53 +0300

    lzkn compressors tests

--- lzkn_info/getOriginalLzknSizes.py
+++ lzkn_info/getOriginalLzknSizes.py
@@ -29,16 +29,16 @@ for line in lines:
     c_uncompressedData = create_string_buffer(MAX_OUT_SIZE)
     decompressedSize = lzkn1.decompress(c_data_p, byref(c_uncompressedData), byref(compressedSize))
     
-    if curAddres == 0x1E597C:
-      with open("map_" + hex(curAddres), "wb") as f:
-          f.write(c_uncompressedData[:decompressedSize])
+    #if curAddres == 0x1E597C:
+    #  with open("map_" + hex(curAddres), "wb") as f:
+    #      f.write(c_uncompressedData[:decompressedSize])
     
     c_recompressedData = create_string_buffer(decompressedSize)
     recompressedSize = lzkn1.compress(byref(c_uncompressedData), byref(c_recompressedData), decompressedSize)
     
-    if curAddres == 0x1E597C:
-      with open("lzkn_" + hex(curAddres), "wb") as f:
-          f.write(c_recompressedData[:recompressedSize])
+    #if curAddres == 0x1E597C:
+    #  with open("lzkn_" + hex(curAddres), "wb") as f:
+    #      f.write(c_recompressedData[:recompressedSize])
     
     print "Lzkn address: %8s"%hex(curAddres),
     print " Decompressed size: %4d"%decompressedSize,
