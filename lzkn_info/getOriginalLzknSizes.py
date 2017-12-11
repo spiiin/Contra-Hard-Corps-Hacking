bc95772e4e277acd50ef4b3012942ce08b008f07
spiiin <sanya.boyko@gmail.com>
2017-12-11 22:03:47 +0300

    check original sizes and sizes after repacking with lab313 lzkn packer

--- lzkn_info/getOriginalLzknSizes.py
+++ lzkn_info/getOriginalLzknSizes.py
@@ -28,7 +28,21 @@ for line in lines:
     c_data_p = POINTER(c_ubyte) (c_data)
     c_uncompressedData = create_string_buffer(MAX_OUT_SIZE)
     decompressedSize = lzkn1.decompress(c_data_p, byref(c_uncompressedData), byref(compressedSize))
-    print "Lzkn address: ", hex(curAddres),
-    print " Decompressed size: ", decompressedSize,
-    print " Compressed size: ", compressedSize.value
+    
+    if curAddres == 0x1E597C:
+      with open("map_" + hex(curAddres), "wb") as f:
+          f.write(c_uncompressedData[:decompressedSize])
+    
+    c_recompressedData = create_string_buffer(decompressedSize)
+    recompressedSize = lzkn1.compress(byref(c_uncompressedData), byref(c_recompressedData), decompressedSize)
+    
+    if curAddres == 0x1E597C:
+      with open("lzkn_" + hex(curAddres), "wb") as f:
+          f.write(c_recompressedData[:recompressedSize])
+    
+    print "Lzkn address: %8s"%hex(curAddres),
+    print " Decompressed size: %4d"%decompressedSize,
+    print " Compressed size: %4d"%compressedSize.value,
+    print " Recompressed size: %4d"%recompressedSize,
+    print " Difference with original: %4d"%(compressedSize.value - recompressedSize)
 
